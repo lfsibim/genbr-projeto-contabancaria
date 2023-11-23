a248f9bebd801517b4177f5794a9b667459696ff
Luis Sibim <141850109+lfsibim@users.noreply.github.com>
2023-11-23 12:29:12 -0300

    add classe conta implementada como classe abstrata

--- conta/model/Conta.java
+++ conta/model/Conta.java
@@ -94,7 +94,7 @@ public abstract class Conta {
 		System.out.println("	Numero da Conta: " + this.numero+"                        ");
 		System.out.println("	Agência: " + this.agencia + "                              ");
 		System.out.println("	Tipo da Conta: " + tipo + "             ");
-		System.out.println("	Titular: " + this.titular + "                          ");
+		System.out.println("	Titular: " + this.titular + "                  ");
 		System.out.println("	Saldo: " + this.saldo+"                            ");
 		System.out.println("**************************************************\n");
 	}
